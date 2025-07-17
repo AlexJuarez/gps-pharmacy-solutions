070af28246492b3419ca779b016b9812610c69e5
Alex Juarez <couatli@gmail.com>
2025-07-16 20:06:35 -0700

    updated wp-polyfill

--- js/main.js
+++ js/main.js
@@ -256,7 +256,7 @@ document.addEventListener('load', () => {
             if (["childList", "subtree"].includes(mutation.type)) {
                 const scripts = document.querySelectorAll('script:not([async="true"])');
                 scripts.forEach((script) => {
-                    if (script.hasAttribute('src') && script.getAttribute('src').toLowerCase().match(/([backbone|i18n|common|mouse|draggable|Marionette|wp|googlesitekit|core])/gi))
+                    if (script.hasAttribute('src') && script.getAttribute('src').toLowerCase().match(/([backbone|i18n|common|mouse|draggable|Marionette|wp-polyfill|googlesitekit|core])/gi))
                         return;
                     console.log(script, `script found without async attribute`);
                     script.setAttribute('async', true);
